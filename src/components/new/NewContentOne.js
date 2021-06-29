6ee09a8c27ea9f4ec6f6b3fdceb863027f378fe3
1Sherlock <nizom702@gmail.com>
2021-06-29 15:07:32 +0500

    Home page completed

--- src/components/new/NewContentOne.js
+++ src/components/new/NewContentOne.js
@@ -7,7 +7,30 @@ import React from 'react';
 const NewContentOne = () => {
     return (
         <div className="new-content new-content-one">
+            <div className="new-content-one-content">
+                <h3 className="font-gilroy-bold">Будущее центров <br/>
+                    оптовой торговли</h3>
 
+                <p className="font-gilroy-medium">
+                    Таргетирование усиливает конструктивный <br/> креатив. Узнавание бренда переворачивает <br/> фирменный охват аудитории
+                </p>
+
+                <div className="d-flex align-items-center">
+                    <button type="button" className="font-gilroy-bold">выбрать помещение </button>
+                    <div className="d-flex align-items-center ">
+                        <div className="play-item">
+                            <img src="/newimages/home-img-2.png" alt="home-img-2.png"/>
+                            <div className="play-item-content">
+                                <span className="icon icon-play"/>
+                            </div>
+                        </div>
+                        <div>
+                            <h4 className="font-gilroy-bold">Ход <br/> строительства</h4>
+                            <h6 className="font-gilroy-bold">Прямой эфир </h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
